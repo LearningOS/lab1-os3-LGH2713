838cd183b52243cdb00899652ee7529bbe839530
LGH2713 <72382960+LGH2713@users.noreply.github.com>
2022-11-22 06:30:11 +0000

    fix: 修复task模块错误

--- os3/src/task/mod.rs
+++ os3/src/task/mod.rs
@@ -12,6 +12,7 @@ pub use task::{TaskControlBlock, TaskStatus};
 
 pub use context::TaskContext;
 
+// 任务控制器
 pub struct TaskManager {
     // 任务总数
     num_app: usize,
@@ -19,6 +20,7 @@ pub struct TaskManager {
     inner: UPSafeCell<TaskManagerInner>,
 }
 
+// 任务控制器内部数据
 pub struct TaskManagerInner {
     // 任务列表
     tasks: [TaskControlBlock; MAX_APP_NUM],
@@ -29,15 +31,18 @@ pub struct TaskManagerInner {
 // 初始化任务管理器
 lazy_static! {
     pub static ref TASK_MANAGER: TaskManager = {
-        let num_app = get_num_app();
+        let num_app = get_num_app(); // 获取应用总数
         let mut tasks = [TaskControlBlock {
             task_cx: TaskContext::zero_init(),
             task_status: TaskStatus::UnInit,
-        }; MAX_APP_NUM];
-        for (i, t) in tasks.iter_mut().enumerate().take(num_app) {
-            t.task_cx = TaskContext::goto_restore(init_app_cx(i));
-            t.task_status = TaskStatus::Ready;
+        }; MAX_APP_NUM]; // 初始化任务列表，每个元素是一个任务控制块
+        for (i, t) in tasks.iter_mut()// 获取可变切片迭代器
+        .enumerate()// 创建一个pair(i, val)迭代器
+        .take(num_app) {
+            t.task_cx = TaskContext::goto_restore(init_app_cx(i)); // 初始化任务上下文
+            t.task_status = TaskStatus::Ready; // 初始化任务状态
         }
+        // 返回一个任务管理器
         TaskManager {
             num_app,
             inner: unsafe {
